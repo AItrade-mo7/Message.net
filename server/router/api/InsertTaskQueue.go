f355c896bc6c8c4550a8ae6597dfcbb9a2a93ffe
墨七 <mo7@mo7.cc>
2023-02-10 15:11:05 +0800

    exit-push

--- server/router/api/InsertTaskQueue.go
+++ server/router/api/InsertTaskQueue.go
@@ -8,6 +8,12 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type TaskQueue struct {
+	TaskID   string         `bson:"TaskID"`   // 任务ID
+	TaskType string         `bson:"TaskType"` // 任务类型  SendEmail, SendOrder 等
+	Content  map[string]any `bson:"Content"`  // 任务内容 用不同的模板去解析
+}
+
 func InsertTaskQueue(c *fiber.Ctx) error {
 	var json map[string]any
 	mFiber.Parser(c, &json)
