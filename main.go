41a29cef55fe79a09240bc37c7d8d720b8285f65
墨七 <mo7@mo7.cc>
2023-02-11 21:10:49 +0800

    exit-push

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"Message.net/server/global"
 	"Message.net/server/global/config"
+	"Message.net/server/ready"
 	"Message.net/server/router"
 	jsoniter "github.com/json-iterator/go"
 )
@@ -17,6 +18,9 @@ func main() {
 
 	// 初始化系统参数
 	global.Start()
+
+	// 主要业务逻辑准备
+	ready.Start()
 
 	// 启动 http 监听服务
 	router.Start()
--- server/ready/Start.go
+++ server/ready/Start.go
@@ -7,4 +7,6 @@ import (
 // 在这里 启动一个子进程，来进行目录的变化监听
 func Start() {
 	fmt.Println("ready.Start")
+
+	ListenFolder()
 }
