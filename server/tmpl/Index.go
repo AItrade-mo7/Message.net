e256a3751f92ea2bde83198be45261ecd9bc17da
墨七 <mo7@mo7.cc>
2023-02-11 17:37:15 +0800

    exit-push

--- server/tmpl/Index.go
+++ server/tmpl/Index.go
@@ -3,3 +3,12 @@ package tmpl
 import (
 	_ "embed"
 )
+
+//go:embed email-sys.html
+var SysEmail string
+
+//go:embed email-code.html
+var CodeEmail string
+
+//go:embed email-register.html
+var RegisterSucceedEmail string
