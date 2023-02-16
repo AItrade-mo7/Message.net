9f381758f22998a00b0bc3939a24982a23fd87f4
墨七 <mo7@mo7.cc>
2023-02-16 17:09:12 +0800

    写入验证码

--- server/disposeTask/EmailCode.go
+++ server/disposeTask/EmailCode.go
@@ -2,8 +2,48 @@ package disposeTask
 
 import (
 	"fmt"
+
+	"Message.net/server/global"
+	"Message.net/server/global/config"
+	"github.com/EasyGolang/goTools/mMongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func UpdateEmailCode(to []string, code string) {
 	fmt.Println("存储验证码", to, code)
+	db := mMongo.New(mMongo.Opt{
+		UserName: config.SysEnv.MongoUserName,
+		Password: config.SysEnv.MongoPassword,
+		Address:  config.SysEnv.MongoAddress,
+		DBName:   "Message",
+	}).Connect().Collection("VerifyCode")
+	defer global.Run.Println("disposeTask.UpdateEmailCode 关闭数据库")
+	defer db.Close()
+
+	FK := bson.D{}
+	UK := bson.D{}
+	for _, val := range to {
+
+		FK = append(FK, bson.E{
+			Key:   val,
+			Value: code,
+		})
+		UK = append(UK, bson.E{
+			Key: "$set",
+			Value: bson.D{
+				{
+					Key:   val,
+					Value: code,
+				},
+			},
+		})
+	}
+
+	upOpt := options.Update()
+	upOpt.SetUpsert(true)
+	_, err := db.Table.UpdateMany(db.Ctx, FK, UK, upOpt)
+	if err != nil {
+		global.LogErr("disposeTask.UpdateEmailCode 数据更插失败", err)
+	}
 }
