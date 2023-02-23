3eccd7c6d1f2491593449b6061061038283fde27
墨七 <mo7@mo7.cc>
2023-02-24 03:00:35 +0800

    登录搞定

--- server/global/dbType/Message.go
+++ server/global/dbType/Message.go
@@ -1,6 +1,10 @@
 package dbType
 
 /*
+db: Message
+*/
+
+/*
 用来存储邮件发送记录
 db: Message
 collection : Email
@@ -21,4 +25,27 @@ type MessageEmail struct {
 	Host        string   `bson:"Host"`
 	TemplateStr string   `bson:"TemplateStr"`
 	SendData    any      `bson:"SendData"`
+}
+
+/*
+用来存储验证码
+db: Message
+collection : VerifyCode
+*/
+type EmailCodeTable struct {
+	Email    string `bson:"Email"`
+	Code     string `bson:"Code"`
+	SendTime int64  `bson:"SendTime"`
+}
+
+/*
+用来存储验证 Token
+db: Message
+collection : VerifyToken
+*/
+
+type TokenTable struct {
+	UserID     string `bson:"UserID"`     // 用户 ID
+	Token      string `bson:"Token"`      // 当前登录的Token
+	CreateTime int64  `bson:"CreateTime"` // 创建时间
 }
