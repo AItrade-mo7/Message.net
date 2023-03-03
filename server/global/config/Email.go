a858590110bc25a170739c99ef3f09ea81019312
墨七 <mo7@mo7.cc>
2023-03-03 14:32:00 +0800

    添加邮箱

--- server/global/config/Email.go
+++ server/global/config/Email.go
@@ -10,6 +10,9 @@ var MyEmailList = []mEmail.ServeType{
 	mEmail.Gmail("mo7trade3@gmail.com", "oinleqxgslppsfbx"),
 	mEmail.Gmail("mo7trade4@gmail.com", "eemopwzayltpxomj"),
 	mEmail.Gmail("mo7trade5@gmail.com", "tzelivngzywfnuar"),
+	mEmail.Gmail("mo7trade6@gmail.com", "wpququxswjjrinnz"),
+	mEmail.Gmail("mo7trade7@gmail.com", "ywmmeeuzjnoekkkj"),
+	mEmail.Gmail("mo7trade8@gmail.com", "baovahfdgmudfeop"),
 	mEmail.Gmail("meichangliang@gmail.com", "pwlooxzamplnwwgf"),
 	// qq
 	mEmail.QQ("mo7trade@qq.com", "aluanmhgxubnbigf"),
