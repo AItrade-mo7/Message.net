7f1ee793a532b56902dcea46a353c2f62a0f9d72
墨七 <mo7@mo7.cc>
2023-04-14 18:18:35 +0800

    x

--- server/router/api/SendEmailCode.go
+++ server/router/api/SendEmailCode.go
@@ -43,11 +43,6 @@ func SendEmailCode(c *fiber.Ctx) error {
 		return c.JSON(result.ErrEmail.WithMsg(emailErr))
 	}
 
-	if len(json.EntrapmentCode) < 1 {
-		emailErr := fmt.Errorf("防钓鱼码不能为空")
-		return c.JSON(result.ErrEmail.WithMsg(emailErr))
-	}
-
 	if len([]rune(json.EntrapmentCode)) > 24 {
 		emailErr := fmt.Errorf("防钓鱼码不能大于24位")
 		return c.JSON(result.ErrEmail.WithMsg(emailErr))
